63370ec28b7b34fd2be2c4e2e0f41203282d135f
Harris Kunwar <iamtheparzival@gmail.com>
2020-10-28 13:41:54 +0545

    Better response and requests

--- oct28/docs/more.py
+++ oct28/docs/more.py
@@ -34,6 +34,17 @@ class Item(BaseModel):
     price: float 
     tax: Optional[float] = None
 
+# Post is used to handle the request bodies.
 @app.post("/items/")
 async def create_item(item: Item):
-    return item
+    item_dict = item.dict()
+
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+@app.put("/items/{item_id}")
+async def put_item(item_id: int, item: Item):
+    # ** operator here is used to unpack the contents of item
+    return {"item_id": item_id, **item.dict()}
